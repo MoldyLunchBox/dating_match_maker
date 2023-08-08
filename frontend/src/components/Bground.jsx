644dbd932872930c69362de189093916bcd21254
MoldyLunchBox <duu116@gmail.com>
2023-08-08 23:45:34 +0100

    update

--- frontend/src/components/Bground.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Bground = () => {
-  return (
-      <div className='font-[Poppings] bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee] h-screen '>
-      </div>
-
-  )
-}
