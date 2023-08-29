1a95125ff5bd04718d852ec1c2aac23842fe4df1
MoldyLunchBox <duu116@gmail.com>
2023-08-29 21:17:13 +0100

    designign the profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import { EditProfil } from './components/pages/EditProfil';
 import { Friends } from './components/pages/Friends';
 import { Home } from './components/pages/Home';
 import { Match } from './components/pages/Match';
+import { Profil } from './components/pages/Profil';
 // import { Home } from 'react-feather';
 
 function App() {
@@ -17,9 +18,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/register" element={<Register />} />
-
         <Route path="/" element={<PrivateRoute element={<Home />} />} />
         <Route path="/login" element={<PrivateRoute element={<Home />} />} />
+        <Route path="/profile" element={<PrivateRoute element={<Profil />} />} />
         <Route path="/Search" element={<PrivateRoute element={<Search />} />} />
         <Route path="/Friends" element={<PrivateRoute element={<Friends />} />} />
         <Route path="/Chat" element={<PrivateRoute element={<Chat />} />} />
