c0db1f082afa6d2a202720275807a76feeed73af
MoldyLunchBox <duu116@gmail.com>
2023-08-31 04:23:18 +0100

    turning figma design to code

--- frontend/src/components/pages/Profil.jsx
+++ frontend/src/components/pages/Profil.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export const Profil = () => {
   return (
-    <div className='flex grow min-h-full bg-red-300'>Profile</div>
+    <div className='flex grow min-h-full gap-3 bg-red-300'>
+      {/* <div class="flex-none  bg-[yellow]">
+    01
+  </div> */}
+  <div class="flex-auto w-32 md:max-w-[600px] bg-[red]">
+   <div className='flex h-full flex-col'>
+   <div class="flex-auto h-14 bg-black">
+    03
+  </div>
+  <div class="flex-auto h-64 bg-[yellow]">
+    03
+  </div>
+   </div>
+  </div>
+  <div class="flex-auto w-64 bg-black hidden md:block">
+    03
+  </div>
+    </div>
   )
 }
