c6ed6c11fa78d1f84bfea81f9e45b5cbbc441b2f
MoldyLunchBox <duu116@gmail.com>
2023-09-01 05:22:09 +0100

    update

--- frontend/src/components/pages/Profil.jsx
+++ frontend/src/components/pages/Profil.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Avatar } from '../profileComponents/Avatar'
 import { LeftCards } from '../profileComponents/LeftCards'
+import { Body } from '../profileComponents/Body'
 
 export const Profil = () => {
+
   return (
     <div className='flex grow min-h-full gap-3  '>
       <div class="flex-none   ">
@@ -16,23 +18,7 @@ export const Profil = () => {
           <LeftCards />
         </div>
       </div>
-      <div class="flex-auto p-10 bg-white justify-center  items-center hidden md:flex">
-        <div className='bg-[#fafafa] border w-full h-full border-[orange] p-10 flex items-center justify-center'>
-          <div className='flex w-full flex-col'>
-            <div className='flex flex-row gap-4  justify-between'>
-              <div className='flex flex-col'>
-                <span>job</span>
-                <span>job</span>
-              </div>
-              <div className='flex flex-col'>
-                <span>interest</span>
-                <span>interest</span>
-                <span>interest</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Body/>
     </div>
   )
 }
---
+++ frontend/src/components/profileComponents/Body.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {   ChevronDown, ChevronUp } from 'react-feather';
+
+export const Body = () => {
+    const interests = [
+        { name: "sports" },
+        { name: "music" },
+        { name: "gaming" },
+    ]
+    return (
+        <div class="flex-auto p-10 bg-white justify-center items-center hidden md:flex">
+            <div className='bg-[#fafafa]   w-full   mytest  p-10 flex items-center justify-center'>
+                <div className='flex w-full h-1/2 gap-10 flex-col'>
+                    <div className='flex flex-row gap-4  justify-between'>
+                        <div className='flex flex-col gap-2'>
+                            <div className='flex items-center gap-1'>
+                                <span className='bg-blue-300 f-montserrat font-semibold rounded-l py-2 px-3 '>nickname</span>
+                                <span className=''>Buger breath</span>
+                            </div>
+                            <div className='flex items-center gap-1'>
+                                <span className='bg-blue-300 f-montserrat font-semibold rounded-l py-2 px-3 '>Job</span>
+                                <span className=''>Web Dev</span>
+                            </div>
+                        </div>
+                        <div className='flex flex-col gap-2 '>
+                            {
+                                interests.map((interest) => (
+                                    <div className='flex flex-row bg-blue-300 justify-between f-montserrat font-semibold rounded-t py-2 px-3 w-[200px]'>
+
+                                    {interest.name} 
+                                    <ChevronDown className='cursor-pointer'/>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <div className='h-1/2 shadow min-h-[300px] p-5 bg-gray-200'>asas</div>
+                </div>
+            </div>
+        </div>
+    )
+}
