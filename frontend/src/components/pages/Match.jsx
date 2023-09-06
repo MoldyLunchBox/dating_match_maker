5c27f3e7cfaa98aee0c1ad2032fc6d592f9c275d
MoldyLunchBox <duu116@gmail.com>
2023-09-06 04:32:50 +0100

    time to designe the matching page

--- frontend/src/components/pages/Match.jsx
+++ frontend/src/components/pages/Match.jsx
@@ -8,7 +8,7 @@ function calculateDistance(lat1, lon1, lat2, lon2) {
         Math.sin(dLat / 2) * Math.sin(dLat / 2) +
         Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c; // Distance in kilometers
+    const distance = R * c; // Distance in kilometerss
     return distance;
 }
 
